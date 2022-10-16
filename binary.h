869f4e32953be811ed11d79dab1d1ceb3fa1540a
saltbourne <saltbourne@gmail.com>
2022-10-15 22:01:31 -0400

    adding binary header

---
+++ binary.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+class Binary
+{
+private:
+	enum Binary_Type
+	{
+		BIN_TYPE_OTHER = 0,
+		BIN_TYPE_ELF   = 1
+	};
+
+	
+}
