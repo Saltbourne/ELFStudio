4919383b60701e8a52e1d3b0c09d7da3fec21d07
saltbourne <saltbourne@gmail.com>
2022-10-23 13:41:53 -0400

    changed binary.h

--- elf/binary.h
+++ elf/binary.h
@@ -9,24 +9,9 @@
 class Binary
 {
 private:
-	enum Binary_Type
-	{
-		BIN_TYPE_OTHER = 0,
-		BIN_TYPE_ELF   = 1
-	};
-
-	enum Binary_Arch 
-	{
-		ARCH_NONE = 0,
-		ARCH_x86 = 1
-	};
-
-	Section *get_text_section();
 
 	std::string		filename;
-	Binary_Type		b_type;
-	std:string		type_string;
-	Binary_Arch		archit;
+	std::string		type_string;
 	std::string 	archit_string;
 	unsigned		bits;
 	uint64_t		entry_addr;
@@ -42,14 +27,34 @@ public:
 		entry_addr = 0x0;
 	}
 
+	enum Binary_Type
+	{
+		BIN_TYPE_OTHER = 0,
+		BIN_TYPE_ELF   = 1
+	};
+
+	enum Binary_Arch 
+	{
+		ARCH_NONE = 0,
+		ARCH_x86 = 1
+	};
+
+	Binary_Type		bin_type;
+	Binary_Arch		bin_arch;
+
+	Section *get_text_section();
+
 	int  load_binary   (std::string &fname, Binary *bin, Binary::BinaryType type);
 	void unload_binary (Binary *bin);
 
-	std::string set_filename();
 	Binary_Type get_Binary_Type();
-	std::string set_type_string();
+	void set_filename(std::string filename);
+	void set_type_string(std::string type_string);
 	void set_archit_string(std::string archit_string)
-	void set_sections_vector(std::vector<Section> &sections);
-	void set_symbols_vector(std::vector<Symbol> &symbols);
+
+	//using pointers for the vector since they can be empty.
+	//References can not be null.
+	void set_sections_vector(std::vector<Section> *sections);
+	void set_symbols_vector(std::vector<Symbol> *symbols);
 
 };
